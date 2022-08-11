3f6a79c076b4055597a1951aaabd620603474b57
reytiartur <101744356+reytiartur@users.noreply.github.com>
2022-08-11 17:32:55 +0200

    Main page with categories

---
+++ src/components/categories-container/categories-container.component.jsx
@@ -0,0 +1,17 @@
+import './categories-container.styles.scss'
+import CategoryItem from '../category-item/category-item.component'
+
+const CategoriesContainer = ({categories}) => {
+
+    return (
+        <div className="categories-container">
+            {categories.map((category) => {
+                return (
+                <CategoryItem key={ category.id } category={ category } />
+                )
+            })}
+        </div>
+    )
+}
+
+export default CategoriesContainer;
